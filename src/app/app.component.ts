b15d9a6462cc72a6d437c3d05bd779997a57aade
汪惠胜(Vesam Wang)-仓配物流业务研发中心 <wanghuisheng@sf-express.com>
2017-03-24 18:34:25 +0800

    study

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+/**
+ * Created by 261685 on 2017/3/17.
+ */
 
+
+import { Component } from '@angular/core';
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+     <a routerLink="/dashboard"  routerLinkActive="active">Dashboard</a>
+     <a routerLink="/heroes">Heroes</a>
+   </nav>
+   <router-outlet></router-outlet>`,
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
---
+++ src/app/hero.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by 261685 on 2017/3/16.
+ */
+
+
+export class Hero{
+  id:number;
+  name:string;
+}
